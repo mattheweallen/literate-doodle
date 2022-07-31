343eec9ee2c8bee4c9505100d1da6e5035c4941c
mattheweallen <mattheweallen@gmail.com>
2022-07-31 15:32:47 -0500

    fix tests.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('/');
   //await expect(page.locator('div div')).toContainText('Hello World');
-  await expect(page.locator('div div')).toHaveClass('game');
+  await expect(page.locator('div')).toHaveClass('game');
 })
