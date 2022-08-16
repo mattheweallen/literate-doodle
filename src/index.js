fd5a319c4a5d97c7220e318b769d21ae2acc5735
mattheweallen <mattheweallen@gmail.com>
2022-08-16 09:47:18 -0500

    lift state up to Board

--- src/index.js
+++ src/index.js
@@ -6,18 +6,40 @@ import './index.css';
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 class Square extends React.Component {
+    
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+          className="square" 
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return( 
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
