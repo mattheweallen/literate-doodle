a38865efb77614857abf546de0a29fad9e125da8
mattheweallen <mattheweallen@gmail.com>
2022-08-28 12:11:52 -0500

    add clock component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Game from './Game';
+import Clock from './Clock';
 
 
 function App() {
   //const value = 'World';
   //return <div>Hello {value}</div>;
-  return <Game/>;
+  return(
+    <div>
+      <Clock/>
+      <Game/>
+    </div>
+  );
 }
 
 export default App;
